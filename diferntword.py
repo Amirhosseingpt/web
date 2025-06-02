bd1f486378797cd4bff7af88e3383a84f578a0b6
Amirhosseingpt <batmangpt9@gmail.com>
2025-06-02 11:50:37 +0330

    Delete diferntword.py
    DeletDiferntword

--- diferntword.py
+++
@@ -1,7 +0,0 @@
-def remove(word):
-    word = word.replace("'" , "").replace('"', "").replace('"', "").replace("'", "").replace(" ", "").replace("?" , "")
-
-    return word
-
-text = "awdakwnawklnf?????'''"
-print(remove(text))
